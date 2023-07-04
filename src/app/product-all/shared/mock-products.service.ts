2b2d7bd489a1a31249c302d6430cda34b1b6806d
Andrei Danila <115635392+bigsosa337@users.noreply.github.com>
2023-07-04 15:22:41 +0300

    corrected the request

--- src/app/product-all/shared/mock-products.service.ts
+++ src/app/product-all/shared/mock-products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
 import { MockProductModel} from "./mock-product.model";
 
 @Injectable({
@@ -18,6 +18,16 @@ export class MockProductsService {
   }
 
   randomMethod(): Observable<any> {
-    return this.httpClient.get<any>(this.url);
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYW5hbmQyNyIsImlhdCI6MTY4ODQ3MTM1MywiZXhwIjoxNjg4NDg5MzUzfQ.hilzy3f8A786zVLwNFFrh_m_y6E50Ald7u-kYB12q8I';
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.httpClient.get<any>(this.url, { headers }).pipe(
+      catchError((error: any) => {
+        console.error('An error occurred:', error);
+        // Handle the error as needed
+        return throwError('Something went wrong');
+      })
+    );
   }
+
 }
