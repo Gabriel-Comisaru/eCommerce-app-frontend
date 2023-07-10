a4055701f6cd330df966b15cd4d6adf91405272b
Andrei Danila <acdnuclear@gmail.com>
2023-07-10 16:53:55 +0300

    Update basket.service.ts

--- src/app/shopping-cart/shared/basket.service.ts
+++ src/app/shopping-cart/shared/basket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {MockProductModel} from "../../product-all/shared/mock-product.model";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { MockProductModel } from "../../product-all/shared/mock-product.model";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -15,44 +15,39 @@ export class BasketService {
   orderItems: any = [];
   private url = 'http://localhost:8081';
 
-
   createOrder(productId: number) {
     const headers = new HttpHeaders()
-      .set('Authorization', `${localStorage.getItem('authorization')}`)
+      .set('Authorization', `${localStorage.getItem('authorization')}`);
     const url = `${this.url}/api/orderItems/${productId}`;
-    this.httpClient.post(url, {"quantity": 1}, {headers: headers}).subscribe()
-    this.orderItems.push(productId)
-    localStorage.setItem('orderItems', JSON.stringify(this.orderItems))
-    return
+    this.httpClient.post(url, { "quantity": 1 }, { headers: headers }).subscribe();
+    this.orderItems.push(productId);
+    localStorage.setItem('orderItems', JSON.stringify(this.orderItems));
+    return;
   }
 
   deleteOrderItem(productId: number) {
-    // const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2QiLCJpYXQiOjE2ODg2NTc1MzksImV4cCI6MTY4ODY3NTUzOX0.q4tXfuEwGPIn9PsaBQlX_Q_PKI9P3NTsXCLE6O0RXyw';
     const headers = new HttpHeaders()
-      .set('Authorization', `${localStorage.getItem('authorization')}`)
-    console.log(productId)
+      .set('Authorization', `${localStorage.getItem('authorization')}`);
+    console.log(productId);
     const url = `${this.url}/api/orderItems/${productId}`;
-    console.log('url ', url)
-    console.log('headers ', headers)
-    return this.httpClient.delete(url, {headers: headers})
-
+    console.log('url ', url);
+    console.log('headers ', headers);
+    return this.httpClient.delete(url, { headers: headers });
   }
 
   getOrderItems(): Observable<any> {
-    // const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2QiLCJpYXQiOjE2ODg2NTc1MzksImV4cCI6MTY4ODY3NTUzOX0.q4tXfuEwGPIn9PsaBQlX_Q_PKI9P3NTsXCLE6O0RXyw'
     const headers = new HttpHeaders().set('Authorization', `${localStorage.getItem('authorization')}`);
     const url = `${this.url}/api/orderItems`;
-    this.orderItems = this.httpClient.get<any>(url, {headers: headers});
-    return this.httpClient.get<any>(url, {headers: headers})
+    this.orderItems = this.httpClient.get<any>(url, { headers: headers });
+    return this.httpClient.get<any>(url, { headers: headers });
   }
-
 
   updateOrderQuantity(productId: number, quantity: number) {
     const headers = new HttpHeaders().set('Authorization', `${localStorage.getItem('authorization')}`);
     const url = `${this.url}/api/orderItems/${productId}/quantity?quantity=${quantity}`;
-    console.log(url)
-    return this.httpClient.put(url, {}, {headers: headers}).subscribe();
+    console.log(url);
+    return this.httpClient.put(url, {}, { headers: headers }).subscribe();
   }
-
-
 }
+
+// Path: src\app\shopping-cart\shared\mock-product.model.ts
