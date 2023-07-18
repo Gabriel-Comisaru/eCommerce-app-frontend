56b5b89c13f6eb7aa9abd61d613523df18cdaac6
Andrei Danila <acdnuclear@gmail.com>
2023-07-18 13:42:44 +0300

    refactored basketpage ts

--- src/app/shopping-cart/basketpage/basketpage.component.ts
+++ src/app/shopping-cart/basketpage/basketpage.component.ts
@@ -4,6 +4,8 @@ import { Product } from "../../home-page/shared/product.model";
 import {ProductsService} from "../../home-page/shared/products.service";
 import {CategoriesService} from "../../product-categories/shared/categories.service";
 import { Item } from "../shared/item.model";
+import {forkJoin} from "rxjs";
+import {combineLatest} from "rxjs/internal/operators/combineLatest";
 
 @Component({
   selector: 'app-basketpage',
@@ -16,6 +18,11 @@ export class BasketpageComponent implements OnInit {
   ) { }
 
   public basketItems: Product[] = [];
+
+  public products: any = [];
+  public orderItemProducts: any = [];
+  public orderedItems: any =[];
+
   loading: boolean = true;
 
   //Map the current quantity of each product
@@ -28,7 +35,7 @@ export class BasketpageComponent implements OnInit {
 
   //Placeholder
   public itemNamesAny: any[] = [];
-
+public lalalala: any[] = [];
 
   orderItems: Array<Item> = [];
   rows: any = [5, 10, 15];
@@ -36,6 +43,30 @@ export class BasketpageComponent implements OnInit {
 
 
   ngOnInit(): void {
+    const productSubscriber = this.productService.getProducts();
+    const orderSubscriber = this.basketService.getOrderItems();
+
+    forkJoin([productSubscriber, orderSubscriber]).subscribe((res: any) => {
+      [this.products, this.orderItemProducts] = res;
+      this.products.forEach((product: any) => {
+        let item = this.orderItemProducts.filter((orderItem: any) => {
+          if (orderItem.productId === product.id) {
+            // console.log('==========================?===========condition met=======================================')
+            // console.log(orderItem.id, product.name, product.categoryName, product.price, product.imagesName[0], orderItem.quantity, product.unitsInStock)
+            this.orderedItems.push({
+              id: orderItem.id,
+              name: product.name,
+              category: product.categoryName,
+              price: product.price,
+              image: product.imagesName[0],
+              quantity: orderItem.quantity,
+              stock: product.unitsInStock
+            })
+          }
+        });
+      })
+    })
+
     this.loading = true;
     this.productService.getProducts().subscribe((list) => {
       this.itemNamesAny = list.map((product: any) => {
@@ -43,7 +74,6 @@ export class BasketpageComponent implements OnInit {
         this.itemPrices.set(product.id, product.price);
         this.itemCategories.set(product.id, product.categoryName);
         this.itemStock.set(product.id, product.unitsInStock);
-
       });
     });
 
@@ -60,6 +90,7 @@ export class BasketpageComponent implements OnInit {
             category: this.itemCategories.get(item.categoryId) || '',
             availableStock: this.itemStock.get(item.productId) || 0,
           };
+          console.log(this.orderItems)
         });
       });
     }, 500)
@@ -69,11 +100,11 @@ export class BasketpageComponent implements OnInit {
     this.loading = false;
     }
 
-  deleteProduct(product: any, index: number, event: any) {
-    this.basketService.deleteOrderItem(product.id).subscribe( (res) => {
-      this.orderItems.splice(index, 1);
-      }
-    )
+  deleteProduct(product: any, event: any) {
+    this.basketService.deleteOrderItem(product.id).subscribe((res) => {
+      this.orderedItems = this.orderedItems.filter((item: any) => item.id !== product.id);
+      this.getTotalPrice();
+    })
   }
 
   checkout() {
@@ -103,7 +134,7 @@ export class BasketpageComponent implements OnInit {
     if (Item.quantity > 1) {
       Item.quantity -= 1;
     } else if (Item.quantity === 1) {
-      this.deleteProduct(Item, 0, null);
+      this.deleteProduct(Item, null);
     }
     this.basketService.updateOrderQuantity(Item.id, Item.quantity)
     this.updateProductQuantityMap()
@@ -113,7 +144,7 @@ export class BasketpageComponent implements OnInit {
 
   getTotalPrice(): string {
     let totalPrice = 0;
-    this.orderItems.forEach((item) => {
+    this.orderedItems.forEach((item: any) => {
       totalPrice += item.price * item.quantity
     });
     return totalPrice.toFixed(2);
