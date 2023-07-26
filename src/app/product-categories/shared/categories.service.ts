bfed2263d6d70020615dc004a293d3829c1fba45
Andrei Danila <acdnuclear@gmail.com>
2023-07-26 12:25:34 +0300

    cleanup

--- src/app/product-categories/shared/categories.service.ts
+++ src/app/product-categories/shared/categories.service.ts
@@ -13,28 +13,11 @@ export class CategoriesService {
   private url = `${BASE_URL_API}/categories`
 
   getCategories(): Observable<any> {
-    // const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYW5hbmQyNyIsImlhdCI6MTY4ODQ3MTM1MywiZXhwIjoxNjg4NDg5MzUzfQ.hilzy3f8A786zVLwNFFrh_m_y6E50Ald7u-kYB12q8I';
-    // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.httpClient.get<any>(this.url).pipe(
       tap((data: any) => {
-        // console.log('Categories received from the backend:', data);
       }),
       catchError((error: any) => {
         console.error('An error occurred:', error);
-        // Handle the error as needed
-        return throwError('Something went wrong');
-      }),
-    );
-  }
-
-  getCategoryById(id: number): Observable<any> {
-    return this.httpClient.get<any>(`${this.url}/${id}`).pipe(
-      tap((data: any) => {
-        // console.log('Category received from the backend:', data);
-      }),
-      catchError((error: any) => {
-        console.error('An error occurred:', error);
-        // Handle the error as needed
         return throwError('Something went wrong');
       }),
     );
