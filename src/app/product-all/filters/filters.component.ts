611151e8f253ea6dc48e5394b1ed7613fc787dd9
Andrei Danila <115635392+bigsosa337@users.noreply.github.com>
2023-07-27 17:24:20 +0300

    bugfix

--- src/app/product-all/filters/filters.component.ts
+++ src/app/product-all/filters/filters.component.ts
@@ -1,72 +1,3 @@
-// import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-// import {CategoriesService} from "../../product-categories/shared/categories.service";
-// import {ActivatedRoute} from "@angular/router";
-//
-// @Component({
-//   selector: 'app-filters',
-//   templateUrl: './filters.component.html',
-//   styleUrls: ['./filters.component.css']
-// })
-// export class FiltersComponent implements OnInit {
-//   // @Input() categories!: string[];
-//   @Output() filtersApplied: EventEmitter<any> = new EventEmitter<any>();
-//   @Output() filtersCleared: EventEmitter<any> = new EventEmitter<any>();
-//   public categories: any[] = [];
-//   selectedCategory!: any;
-//   selectedPriceRange!: [number, number];
-//
-//
-//   constructor(private categoryService: CategoriesService,
-//               private route: ActivatedRoute
-//               ) { }
-//
-//
-//   ngOnInit(): void {
-//       this.loadData()
-//     this.applyFilters()
-//   }
-//   loadData(): void {
-//     this.selectedPriceRange = [0, 1000];
-//     this.categoryService.getCategories().subscribe((list) => {
-//       this.categories = list;
-//       const categoryIdParam = this.route.snapshot.params['category'];
-//       console.log(categoryIdParam, '----p-a-r-a-m---------------')
-//       if (categoryIdParam) {
-//         this.selectedCategory = this.categories.find((category: any) => category.id == categoryIdParam);
-//         console.log(this.selectedCategory, '-----------------1---1--1-')
-//       }
-//     });
-//     console.log(this.selectedCategory)
-//   }
-//
-//   onCategoryChange(category: any) {
-//     this.selectedCategory = category;
-//     console.log(this.selectedCategory)
-//   }
-//
-//
-//   applyFilters(): void {
-//     const selectedCategoryId = this.selectedCategory ? this.selectedCategory.id : '';
-//     console.log(this.selectedCategory, '------====-----------------------')
-//     const selectedPriceMin = this.selectedPriceRange[0];
-//     const selectedPriceMax = this.selectedPriceRange[1];
-//
-//     // Emit the filter values
-//     this.filtersApplied.emit({
-//       categoryId: selectedCategoryId,
-//       priceMin: selectedPriceMin,
-//       priceMax: selectedPriceMax
-//     });
-//   }
-//   clearFilters(): void {
-//     this.selectedCategory = '';
-//     this.selectedPriceRange = [0, 1000];
-//     this.filtersCleared.emit({selectedCategory: this.selectedCategory,
-//       selectedPriceRange: this.selectedPriceRange});
-//   }
-//
-//
-// }
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CategoriesService } from "../../product-categories/shared/categories.service";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -102,9 +33,10 @@ export class FiltersComponent implements OnInit {
     this.categoryService.getCategories().subscribe((list) => {
       this.categories = list;
       const categoryIdParam = this.route.snapshot.queryParams;
+      console.log(categoryIdParam)
       if (categoryIdParam) {
         this.selectedCategory = this.categories.find((category: any) => category.id == categoryIdParam['categoryId']);
-        // console.log(this.selectedCategory, '-----------------1---1--1-')
+        console.log(this.selectedCategory, '-----------------1---1--1-')
         this.filtersApplied.emit({
           categoryId: this.selectedCategory != undefined ? this.route.snapshot.queryParams['categoryId'] : '',
           priceMin: 0,
