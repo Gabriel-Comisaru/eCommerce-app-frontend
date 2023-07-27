f2d992781083ae0e313b54e9270982966b5bd4f4
johnn93 <112646294+johnn93@users.noreply.github.com>
2023-07-27 17:48:48 +0300

    Feature/admin refactor (#93)
    * some bug fixing
    
    * changed the input with the primeNg input for images in add product, rewrote some code, did the pagination bug
    
    * refactor
    
    * refactor
    
    * changes on update product modal and catching errors on delete button admin product list
    
    * refactor
    
    * made navbar on admin page
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { ProductsService } from './home-page/shared/products.service';
-import { NavigationEnd, Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {ProductsService} from './home-page/shared/products.service';
+import {ActivatedRouteSnapshot, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
+import {UserService} from "./services/user.service";
 import { filter } from 'rxjs';
 
 @Component({
