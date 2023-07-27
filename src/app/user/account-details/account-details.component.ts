abb0d74817a0e9be8fb7d9019039b57fa5f2064c
Laurrx <c.lauralex1@gmail.com>
2023-07-27 17:50:00 +0300

    #86 WIP and change admin dashboard button

--- src/app/user/account-details/account-details.component.ts
+++ src/app/user/account-details/account-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
+import { AdressServiceService } from 'src/app/shopping-cart/shared/adress-service.service';
 
 @Component({
   selector: 'app-account-details',
@@ -12,11 +14,45 @@ export class AccountDetailsComponent implements OnInit{
     localStorage.getItem('currentUser') || '{}'
   );
 
-  constructor(private userService: UserService) {}
+  userAddressForm = this.fb.group({
+    phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+    county: ['', [Validators.required]],
+    city: ['', [Validators.required]],
+    address: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]]
+  });
+  counties: any = [];
+  cities: any = [];
+
+  constructor(private userService: UserService,
+              private addressService: AdressServiceService,
+              private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.userService.getLoggedUserObservable().subscribe((res) => {
       this.loggedInUser = res;
     });
+    this.getCounties();
+    this.userAddressForm.controls.city.disable();
+  }
+
+  getCounties() {
+    this.addressService.getCounties().subscribe((list: any) => {
+      this.counties = list;
+      console.log(list);
+    });
+  }
+
+  getCities() {
+    this.userAddressForm.controls.city.enable();
+
+    this.addressService
+      .getCities(this.userAddressForm.controls.county.value!)
+      .subscribe((list: any) => {
+        this.cities = list;
+      });
+  }
+
+  onSubmitAddress() {
+    console.log(this.userAddressForm.value); 
   }
 }
